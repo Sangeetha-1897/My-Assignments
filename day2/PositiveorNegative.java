131c10bacda25a7e7f6d7f33223b67d638a3fceb
Sangeetha-1897 <126379216+Sangeetha-1897@users.noreply.github.com>
2023-03-11 00:14:56 +0530

    Add files via upload

---
+++ day2/PositiveorNegative.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class PositiveorNegative {
+	public static void main(String[] args) {
+int a=35;
+if(a==0)
+{
+	System.out.println("the number is neither positive nor negative");
+}
+if(a>0)
+{
+	System.out.println("the number is positive");
+}
+
+else {
+	System.out.println("the number is negative");
+}
+	
+}
+
+
+}
---
+++ day2/SecondSmallest.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class SecondSmallest {
+
+	public static void main(String[] args) {
+		int[] num= {23,45,67,32,89,22};
+
+		for (int i = 0; i < num.length; i++) {
+			
+			System.out.println(num[i]);
+
+		}
+		System.out.println("The second smallest number is "+ num[1]);
+	}
+
+}
